8ecd0cc3d8b578cc047e278438e2cff402d03666
Alex Lutz <alexinslc@gmail.com>
2015-02-09 21:27:49 -0700

    Updated settings.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'hackUSU' });
 });
 
-/* GET hackmit page. */
+/* GET hackmit page.
 router.get('/hackmit', function(req, res, next) {
   res.render('hackmit', { title: 'HackMIT' });
 });
+*/
 
 module.exports = router;
