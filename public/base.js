252bb159cff93ae8639473ebba1190b8afc5e20a
Alex Lutz <alexinslc@gmail.com>
2015-02-11 09:24:10 -0700

    Fixed mobile layout.

--- public/base.js
+++ public/base.js
@@ -1,23 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  $('.main')
+      .transition('fade in');
+  $('.ui.dropdown')
+      .dropdown();
 
-  if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
-    skrollr.init({
-      forceHeight: false
-    });
+  var $dimmer = $('.ui.page.dimmer');
+
+  window.dimmerMessage = function(title, subtitle, callback, timeout) {
+    $dimmer.find('h1').html(title);
+    $dimmer.find('h2').html(subtitle);
+    $dimmer
+      .dimmer('show');
+    setTimeout(function(){
+      $dimmer.dimmer('hide');
+      callback();
+    }, timeout)
   }
-
-  $('#main')
-    .transition('fade up in', 2000);
-
-  $("#next").click(function() {
-    $('html, body').animate({
-      scrollTop: $("#info").offset().top
-    }, 1000);
-  });
-
-  $('#c')
-    .click(function(){
-      location.href = '/6361746d6974'
-    })
 
 });
